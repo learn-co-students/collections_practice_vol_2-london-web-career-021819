206b147498c8f8b3319ab4aa850d32d3216ad0a4
Ali Madni <ali555m@hotmail.com>
2019-02-14 00:03:15 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -36,10 +36,45 @@ def remove_non_strings(array)
 end
 
 def count_elements(array)
-  new_array = []
-  array.each do |hash|
-    if !(new_array[hash])
-      new_array[hash]
+  new_array = array.uniq
+  new_array.each do |element|
+    element[:count] = array.count(element)
+  end
+  new_array
+end
+
+def merge_data(keys, data)
+  merged_data = []
+  keys.each do |key|
+    data.each do |person|
+      key.merge!(person[key[:first_name]])
+      merged_data << key
     end
   end
+  merged_data
+end
+
+
+def find_cool(data)
+  dat = []
+  data.each do |key|
+    if key.values.include?("cool")
+      dat << key
+    end
+  end
+  dat
+end
+
+def organize_schools(schools)
+  new_hash = {}
+  schools.each do |school, location|
+    local = location[:location]
+    if !(new_hash[local])
+      new_hash[local] = []
+      new_hash[local] << school
+    else
+      new_hash[local] << school
+    end
+  end
+  new_hash
 end
